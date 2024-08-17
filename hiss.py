adac7f0e027c13262bb2e13b2561a10439c2c5e9
Michael Myers <michael.myers@wustl.edu>
2024-08-17 13:30:26 -0500

    lengthened hiss minimum duration to avoid inadvertent key presses

--- hiss.py
+++ hiss.py
@@ -7,7 +7,7 @@ ctx = Context()
 
 
 start = 0
-noise_length = 0.2
+noise_length = 0.32
 
 
 # global last_activity_at
@@ -18,7 +18,7 @@ hiss_end_time   = 0
 
 
 # short: Single Click
-min_dot_time = 0.2
+min_dot_time = 0.32
 # Medium: Double Click
 max_dot_time = 1.0
 ludicrous_time = 2.0
