bb0dfeea2677db2731ef2b810cd30cf1b5de5999
michael.myers@wustl.edu <m@admins-MacBook-Pro.local>
2024-12-16 19:24:37 -0600

    key_hold now accepts now accepts repeat rate and delay args

--- key_hold.py
+++ key_hold.py
@@ -1,28 +1,21 @@
 from talon import Module, actions, cron
 
-# from https://github.com/AndreasArvidsson/andreas-talon/blob/2772b176f2c5679d8da4ffeba75db1eda20b711a/core/keys/key_hold.py
-
-
-REPEAT_DELAY = "250ms"
-REPEAT_RATE = "16ms"
+# adapted from https://github.com/AndreasArvidsson/andreas-talon/blob/2772b176f2c5679d8da4ffeba75db1eda20b711a/core/keys/key_hold.py
 
 mod = Module()
 repeated_key_jobs = {}
 
-
 @mod.action_class
 class Actions:
-    def key_hold(key: str):
+    def key_hold(key: str, repeat_rate: str = "16ms", repeat_delay: str = "256ms"):
         """Simulate holding a key with repeated key presses"""
         actions.key(key)
-
         def add_interval():
             repeated_key_jobs[key] = cron.interval(
-                REPEAT_RATE,
+                repeat_rate,
                 lambda: actions.key(key),
             )
-
-        repeated_key_jobs[key] = cron.after(REPEAT_DELAY, add_interval)
+        repeated_key_jobs[key] = cron.after(repeat_delay, add_interval)
 
     def key_release(key: str):
         """Stop repeating key"""
