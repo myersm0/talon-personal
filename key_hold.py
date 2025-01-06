f4bffc126d0eb29f1b9c18bd4278ed4b1acb91a4
michael.myers@wustl.edu <m@Mac.attlocal.net>
2025-01-05 21:44:11 -0600

    gpt improvement to key_hold that hopefully solves the problem of confused key holds and releases when a diagonal on the dpad is accidentally pressed

--- key_hold.py
+++ key_hold.py
@@ -1,7 +1,5 @@
 from talon import Module, actions, cron
 
-# adapted from https://github.com/AndreasArvidsson/andreas-talon/blob/2772b176f2c5679d8da4ffeba75db1eda20b711a/core/keys/key_hold.py
-
 mod = Module()
 repeated_key_jobs = {}
 
@@ -9,17 +7,29 @@ repeated_key_jobs = {}
 class Actions:
     def key_hold(key: str, repeat_rate: str = "16ms", repeat_delay: str = "256ms"):
         """Simulate holding a key with repeated key presses"""
+        if key in repeated_key_jobs:
+            # If already holding the key, do nothing
+            return
         actions.key(key)
+
         def add_interval():
             repeated_key_jobs[key] = cron.interval(
                 repeat_rate,
                 lambda: actions.key(key),
             )
+
+        # Schedule the initial delay followed by repeated presses
         repeated_key_jobs[key] = cron.after(repeat_delay, add_interval)
 
     def key_release(key: str):
         """Stop repeating key"""
-        job = repeated_key_jobs[key]
-        if job is not None:
-            cron.cancel(job)
-            repeated_key_jobs[key] = None
+        if key in repeated_key_jobs:
+            job = repeated_key_jobs.pop(key, None)
+            if job is not None:
+                cron.cancel(job)
+
+    def release_all_keys():
+        """Stop all repeating keys"""
+        for key in list(repeated_key_jobs.keys()):
+            self.key_release(key)
+
