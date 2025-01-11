96dd9d9a15c30fcf18638d2e4efd003314419bda
Michael Myers <michael.myers@wustl.edu>
2025-01-11 09:41:17 -0600

    fixed gamepad buttons that were swapped accidentally in julia app

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -392,12 +392,12 @@ class GeneralActions:
         actions.user.key_release(".")
 
     def gamepad_press_left_shoulder():
-        actions.key("ctrl-pageup")
+        actions.key("ctrl-pagedown")
     def gamepad_release_left_shoulder(held):
         actions.skip()
 
     def gamepad_press_right_shoulder():
-        actions.key("ctrl-pagedown")
+        actions.key("ctrl-pageup")
     def gamepad_release_right_shoulder(held):
         actions.skip()
 
