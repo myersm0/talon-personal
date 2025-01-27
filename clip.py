9179fccb9e71155daf561c9233c1655abc37ea86
Michael Myers <michael.myers@wustl.edu>
2025-01-27 10:30:30 -0600

    in clip function added including the source (window title) at the top of the output file

--- clip.py
+++ clip.py
@@ -1,6 +1,6 @@
 import os
 import time
-from talon import Module, actions, clip
+from talon import Module, actions, clip, ui
 
 mod = Module()
 
@@ -9,6 +9,8 @@ class Actions:
     def save_selected_text_to_file():
         """Save selected text from browser to a file with a timestamp"""
         selected_text = actions.edit.selected_text() or clip.text()
+        selected_from = ui.active_window().title
+        text_to_paste = f"# source: {selected_from}\n{selected_text}"
 
         if not selected_text:
             actions.app.notify("No text selected")
@@ -20,7 +22,7 @@ class Actions:
         file_path = os.path.join(folder_path, f"{timestamp}.txt")
 
         with open(file_path, "w") as f:
-            f.write(selected_text)
+            f.write(text_to_paste)
 
         actions.app.notify(f"Text saved to {file_path}")
 
