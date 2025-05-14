b2847f32a16fbd0e4f7450489b7cac7de179f154
Michael Myers <michael.myers@wustl.edu>
2025-05-14 09:29:22 -0500

    in hiss.py, reduced false positives by enforcing a max hiss duration and also enforcing that the mouse has to move between actions

--- core/hiss.py
+++ core/hiss.py
@@ -1,4 +1,4 @@
-from talon import Module, actions, noise
+from talon import Module, actions, noise, ctrl
 import time
 
 # an adaptive twist on using the hiss noise
@@ -13,10 +13,14 @@ def hiss_action():
 # arbitrary-length lists of thresholds to apply to your hiss action
 thresholds  = [0.8, 0.62, 0.45, 0.27, 0.15]
 
+max_hiss_dur = 1.5
+
 # decay times, in seconds, for dropping from level i to level i-1
 # (expected to match length of threshold list above)
-# (index 0 will be unused, so put a dummy 0.0 at start)
-decay_times = [0.0, 300.0, 30.0, 10.0, 3.0]
+# (index 0 will be unused)
+decay_times = [None, 300.0, 30.0, 10.0, 3.0]
+
+last_mouse_pos = ctrl.mouse_pos()
 
 # state
 hiss_stage = 0
@@ -39,7 +43,7 @@ class Actions:
         - decrement hiss_stage if enough time has passed
         - measure hiss_length and act accordingly
         """
-        global hiss_stage, last_action_time
+        global hiss_stage, last_action_time, last_mouse_pos
 
         now: float = time.monotonic()
         hiss_length: float = now - hiss_start_time
@@ -58,11 +62,18 @@ class Actions:
         threshold = thresholds[hiss_stage]
         print(f"[hiss] threshold = {threshold:.3f}")
 
-        if hiss_length >= threshold:
+        if hiss_length > max_hiss_dur:
+            print(f"[hiss] action rejected (hiss was too long)")
+        elif hiss_length > threshold:
+            current_mouse_pos = ctrl.mouse_pos()
+            if current_mouse_pos == last_mouse_pos:
+                print(f"[hiss] action rejected (the mouse has not moved)")
+                return
             hiss_action()
             hiss_stage = min(hiss_stage + 1, len(thresholds) - 1)
             print(f"[hiss] advance stage to {hiss_stage}")
             last_action_time = now
+            last_mouse_pos = ctrl.mouse_pos()
         else:
             print("[hiss] no click (too short)")
 
